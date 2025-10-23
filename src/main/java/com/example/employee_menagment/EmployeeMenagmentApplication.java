b228fcb53e3e77e4740068c149f8eac247f16945
Samet Ruzgar <sametruzgar.2804@gmail.com>
2025-10-23 12:28:59 +0300

    Initial commit

---
+++ src/main/java/com/example/employee_menagment/EmployeeMenagmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee_menagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMenagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMenagmentApplication.class, args);
+	}
+
+}
